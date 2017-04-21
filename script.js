e0c7f477797a28ee8db5edc0bf01a1f14c95aa78
Ketshi <kbavishi@id.iit.edu>
2017-04-21 10:17:56 -0500

    Made the image using javascript.

--- script.js
+++ script.js
@@ -1,2 +1,44 @@
 // Do your work in this file.
 
+
+var view = document.createElement("div");
+view.className = 'view';
+document.body.appendChild(view);
+
+
+var grass = document.createElement("div");
+grass.className = 'grass';
+view.appendChild(grass);
+
+
+var sun = document.createElement("div");
+sun.className = 'sun';
+view.appendChild(sun);
+
+
+var i = 0;
+while (i < 5) {
+    var ray = document.createElement("div");
+    ray.className = 'ray-' + (i + 1);
+    sun.appendChild(ray);
+    i++;
+}
+
+var mountain = document.createElement("div");
+mountain.className = 'mountain';
+view.appendChild(mountain);
+
+var mountainTop = document.createElement("div");
+mountainTop.className = 'mountain-top';
+mountain.appendChild(mountainTop);
+
+var i = 0;
+do {
+    var mountainCap = document.createElement("div");
+    mountainCap.className = 'mountain-cap-' + (i + 1);
+    mountainTop.appendChild(mountainCap);
+    i++;
+} while (i < 3);
+
+
+
